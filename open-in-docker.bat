f8a88d0e700b3dd7558ffc4ac147ab1fdca819c1
DESKTOP-GG7LDTG\iceyu <iceyuool@gmail.com>
2022-04-13 12:52:19 +0800

    add README-Zh

--- open-in-docker.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set IMAGE_NAME=khronosgroup/docker-images:openxr.202110
-set MOUNTPOINT=%cd%
-
-
-
-docker run -it --rm  --mount type=bind,source="%MOUNTPOINT%",target=/openxr %IMAGE_NAME% %*
